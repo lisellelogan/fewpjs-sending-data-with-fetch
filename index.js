1f33273b7f3b93c5ae564ac4970916fe7976940f
lisellelogan <liselle.logan@yahoo.com>
2021-04-04 11:54:35 -0400

    commit tree 0f6a57fa848a07d3d46bc5a901084d782a252f2d

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Add your code here
+ // write method submitData that takes two strings as argument
+ // string one = user's name
+ // string two = user's email
+ // returns fetch to access requests inside submitData
+
+ function submitData(name, email){
+    return fetch("http://localhost:3000/users", {
+        method: "POST",
+        headers: {
+         "Content-Type": "application/json",
+         "Accept": "application/json" 
+         },
+        body: JSON.stringify({
+            name: name,
+            email: email
+        })
+    })
+    .then(function(response) {
+        return response.json();
+    })
+    .then(function(object){
+        document.body.innerHTML = object.id;
+    })
+    .catch(function(error){
+        document.body.innerHTML = error.message;
+    });
+ }
